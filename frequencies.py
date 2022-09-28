fff48786e86e74179d88dfaec261164ed2bd7ace
min jung <min.jung@kcl.ac.uk>
2022-09-28 17:57:01 +0100

    add frequency counter based on list

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        key = str(item)
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
     return frequencies
